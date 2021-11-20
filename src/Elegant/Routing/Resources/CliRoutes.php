7494be1404eeeb17b4737377374668d6a8db9ec5
giorgosstab <tsaxre1991@hotmail.com>
2021-11-20 11:42:57 +0200

    Add elegant route package

---
+++ src/Elegant/Routing/Resources/CliRoutes.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * CLI Routes
+ *
+ * This routes only will be available under a CLI environment
+ */
+
+// To enable laraigniter built-in cli commands
+// uncomment the followings lines:
+
+// Elegant\Console\Command::maker();
+// Elegant\Database\Console\Migrations::migrations();
---
+++ src/Elegant/Support/Facades/Route.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Elegant\Support\Facades;
+
+use Elegant\Routing\RouteBuilder;
+
+class Route extends RouteBuilder
+{
+}
