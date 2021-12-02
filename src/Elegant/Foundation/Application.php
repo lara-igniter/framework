a48d47862a42f170050e67280c4ebc5ab7568ef7
giorgosstab <tsaxre1991@hotmail.com>
2021-12-02 20:48:14 +0200

    Add Application class for environment

---
+++ src/Elegant/Foundation/Application.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Elegant\Foundation;
+
+use Dotenv\Dotenv;
+use Elegant\Support\Env;
+
+class Application
+{
+    /**
+     * The Laraigniter framework version.
+     *
+     * @var string
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * The base path for the Laraigniter installation.
+     *
+     * @var string
+     */
+    protected $basePath;
+
+    /**
+     * The custom environment path defined by the developer.
+     *
+     * @var string
+     */
+    protected $environmentPath;
+
+    /**
+     * The environment file to load during bootstrapping.
+     *
+     * @var string
+     */
+    protected $environmentFile = '.env';
+
+    public function __construct($basePath = null)
+    {
+        if ($basePath) {
+            $this->setBasePath($basePath);
+        }
+
+        $this->createDotenv();
+    }
+
+    /**
+     * Set the base path for the application.
+     *
+     * @param  string  $basePath
+     * @return $this
+     */
+    public function setBasePath($basePath)
+    {
+        $this->basePath = rtrim($basePath, '\/');
+
+        return $this;
+    }
+
+    public function environmentPath()
+    {
+        return $this->environmentPath ?: $this->basePath;
+    }
+
+    /**
+     * Get the environment file the application is using.
+     *
+     * @return string
+     */
+    public function environmentFile()
+    {
+        return $this->environmentFile ?: '.env';
+    }
+
+    protected function createDotenv()
+    {
+        return Dotenv::create(
+            Env::getRepository(),
+            $this->environmentPath(),
+            $this->environmentFile()
+        );
+    }
+}
