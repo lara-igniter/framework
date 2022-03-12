c9d13839afae3f616271214ecdfdea11a2094c0c
giorgosstab <tsaxre1991@hotmail.com>
2022-03-12 15:32:00 +0200

    Add filesystem package

---
+++ src/Elegant/Filesystem/Filesystem.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Elegant\Filesystem;
+
+use Elegant\Contracts\Filesystem\FileNotFoundException;
+use ErrorException;
+use Exception;
+
+class Filesystem
+{
+    /**
+     * Determine if a file exists.
+     *
+     * @param string $path
+     * @return bool
+     */
+    public function exists($path)
+    {
+        return file_exists($path);
+    }
+
+    /**
+     * Get the contents of a file.
+     *
+     * @param string $path
+     * @return string
+     *
+     * @throws Exception
+     */
+    public function get($path)
+    {
+        if ($this->isFile($path)) {
+            return file_get_contents($path);
+        }
+
+        throw new Exception("File does not exist at path {$path}");
+    }
+
+    /**
+     * Get the returned value of a file.
+     *
+     * @param string $path
+     * @return mixed
+     *
+     * @throws FileNotFoundException
+     */
+    public function getRequire($path)
+    {
+        if ($this->isFile($path)) {
+            return require $path;
+        }
+
+        throw new FileNotFoundException("File does not exist at path {$path}");
+    }
+
+    /**
+     * Write the contents of a file.
+     *
+     * @param string $path
+     * @param string $contents
+     * @param bool $lock
+     * @return int
+     */
+    public function put($path, $contents, $lock = false)
+    {
+        return file_put_contents($path, $contents, $lock ? LOCK_EX : 0);
+    }
+
+    /**
+     * Write the contents of a file, replacing it atomically if it already exists.
+     *
+     * @param string $path
+     * @param string $content
+     * @return void
+     */
+    public function replace($path, $content)
+    {
+        // If the path already exists and is a symlink, get the real path...
+        clearstatcache(true, $path);
+
+        $path = realpath($path) ?: $path;
+
+        $tempPath = tempnam(dirname($path), basename($path));
+
+        // Fix permissions of tempPath because `tempnam()` creates it with permissions set to 0600...
+        chmod($tempPath, 0777 - umask());
+
+        file_put_contents($tempPath, $content);
+
+        rename($tempPath, $path);
+    }
+
+    /**
+     * Delete the file at a given path.
+     *
+     * @param string|array $paths
+     * @return bool
+     */
+    public function delete($paths)
+    {
+        $paths = is_array($paths) ? $paths : func_get_args();
+
+        $success = true;
+
+        foreach ($paths as $path) {
+            try {
+                if (!@unlink($path)) {
+                    $success = false;
+                }
+            } catch (ErrorException $e) {
+                $success = false;
+            }
+        }
+
+        return $success;
+    }
+
+    /**
+     * Get the file's last modification time.
+     *
+     * @param string $path
+     * @return int
+     */
+    public function lastModified($path)
+    {
+        return filemtime($path);
+    }
+
+    /**
+     * Determine if the given path is a file.
+     *
+     * @param string $file
+     * @return bool
+     */
+    public function isFile($file)
+    {
+        return is_file($file);
+    }
+
+    /**
+     * Find path names matching a given pattern.
+     *
+     * @param string $pattern
+     * @param int $flags
+     * @return array
+     */
+    public function glob($pattern, $flags = 0)
+    {
+        return glob($pattern, $flags);
+    }
+}
