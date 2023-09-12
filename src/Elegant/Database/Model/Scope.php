5a8e3867b5b09c0438a3ce7ad9af36a817e00700
George Tsachrelias <tsaxre1991@hotmail.com>
2023-09-12 12:48:27 +0300

    feature: Update database package

---
+++ src/Elegant/Database/Model/Scope.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Elegant\Database\Model;
+
+use \CI_DB_mysqli_driver;
+
+interface Scope
+{
+    /**
+     * Apply the scope to a given model query builder.
+     *
+     * @param \CI_DB_mysqli_driver $builder
+     * @return void
+     */
+    public function apply(CI_DB_mysqli_driver $builder);
+}
