121dd5ab87cf341df725d1f093ce2ee6fb4bd992
George Tsachrelias <tsaxre1991@hotmail.com>
2023-10-16 00:21:46 +0300

    feature: Add new facade for http client factory
    - Add comment for http factory class

--- src/Elegant/Http/Client/Factory.php
+++ src/Elegant/Http/Client/Factory.php
@@ -6,6 +6,47 @@ use Closure;
 use GuzzleHttp\Psr7\Response as Psr7Response;
 use Elegant\Support\Str;
 use Elegant\Support\Traits\Macroable;
+
+/**
+ * @method \Elegant\Http\Client\PendingRequest accept(string $contentType)
+ * @method \Elegant\Http\Client\PendingRequest acceptJson()
+ * @method \Elegant\Http\Client\PendingRequest asForm()
+ * @method \Elegant\Http\Client\PendingRequest asJson()
+ * @method \Elegant\Http\Client\PendingRequest asMultipart()
+ * @method \Elegant\Http\Client\PendingRequest async()
+ * @method \Elegant\Http\Client\PendingRequest attach(string|array $name, string $contents = '', string|null $filename = null, array $headers = [])
+ * @method \Elegant\Http\Client\PendingRequest baseUrl(string $url)
+ * @method \Elegant\Http\Client\PendingRequest beforeSending(callable $callback)
+ * @method \Elegant\Http\Client\PendingRequest bodyFormat(string $format)
+ * @method \Elegant\Http\Client\PendingRequest contentType(string $contentType)
+ * @method \Elegant\Http\Client\PendingRequest dd()
+ * @method \Elegant\Http\Client\PendingRequest dump()
+ * @method \Elegant\Http\Client\PendingRequest retry(int $times, int $sleep = 0)
+ * @method \Elegant\Http\Client\PendingRequest sink(string|resource $to)
+ * @method \Elegant\Http\Client\PendingRequest stub(callable $callback)
+ * @method \Elegant\Http\Client\PendingRequest timeout(int $seconds)
+ * @method \Elegant\Http\Client\PendingRequest withBasicAuth(string $username, string $password)
+ * @method \Elegant\Http\Client\PendingRequest withBody(resource|string $content, string $contentType)
+ * @method \Elegant\Http\Client\PendingRequest withCookies(array $cookies, string $domain)
+ * @method \Elegant\Http\Client\PendingRequest withDigestAuth(string $username, string $password)
+ * @method \Elegant\Http\Client\PendingRequest withHeaders(array $headers)
+ * @method \Elegant\Http\Client\PendingRequest withMiddleware(callable $middleware)
+ * @method \Elegant\Http\Client\PendingRequest withOptions(array $options)
+ * @method \Elegant\Http\Client\PendingRequest withToken(string $token, string $type = 'Bearer')
+ * @method \Elegant\Http\Client\PendingRequest withUserAgent(string $userAgent)
+ * @method \Elegant\Http\Client\PendingRequest withoutRedirecting()
+ * @method \Elegant\Http\Client\PendingRequest withoutVerifying()
+ * @method array pool(callable $callback)
+ * @method \Elegant\Http\Client\Response delete(string $url, array $data = [])
+ * @method \Elegant\Http\Client\Response get(string $url, array|string|null $query = null)
+ * @method \Elegant\Http\Client\Response head(string $url, array|string|null $query = null)
+ * @method \Elegant\Http\Client\Response patch(string $url, array $data = [])
+ * @method \Elegant\Http\Client\Response post(string $url, array $data = [])
+ * @method \Elegant\Http\Client\Response put(string $url, array $data = [])
+ * @method \Elegant\Http\Client\Response send(string $method, string $url, array $options = [])
+ *
+ * @see \Elegant\Http\Client\PendingRequest
+ */
 class Factory
 {
     use Macroable {
