4745934b5720c692feda7630a2280bdf955d756c
George Tsachrelias <tsaxre1991@hotmail.com>
2023-12-21 08:49:13 +0200

    refactor: version

--- src/Elegant/Foundation/Application.php
+++ src/Elegant/Foundation/Application.php
@@ -12,7 +12,7 @@ class Application
      *
      * @var string
      */
-    const VERSION = '1.19.0';
+    const VERSION = '1.19.1';
 
     /**
      * The base path for the Laraigniter installation.
