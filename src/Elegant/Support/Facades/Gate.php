ae112018876cc48d90bb46dc90efa3ec911beb94
George Tsachrelias <tsaxre1991@hotmail.com>
2024-04-16 13:54:30 +0300

    feat: Add Gate facade & update application version

---
+++ src/Elegant/Support/Facades/Gate.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Elegant\Support\Facades;
+
+use Elegant\Contracts\Auth\Access\Gate as GateContract;
+
+/**
+ * @method static bool|void authorize(string $ability, array|mixed $arguments = [])
+ * @method static bool allows(string $ability, array|mixed $arguments = [])
+ * @method static bool any(iterable|string $abilities, array|mixed $arguments = [])
+ * @method static bool check(iterable|string $abilities, array|mixed $arguments = [])
+ * @method static bool denies(string $ability, array|mixed $arguments = [])
+ * @method static mixed raw(string $ability, array|mixed $arguments = [])
+ *
+ * @see \Elegant\Contracts\Auth\Access\Gate
+ */
+class Gate extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return GateContract::class;
+    }
+}
