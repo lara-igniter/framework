57b6f88a1a2f42a43f0265a2385f379507d3d2ea
George Tsachrelias <tsaxre1991@hotmail.com>
2024-09-13 15:07:24 +0300

    fix: Typo syntax $this->app config mail

--- src/Elegant/Mail/MailManager.php
+++ src/Elegant/Mail/MailManager.php
@@ -366,7 +366,7 @@ class MailManager implements FactoryContract
      */
     protected function setGlobalAddress($mailer, array $config, string $type)
     {
-        $address = Arr::get($config, $type, $this->app['config']['mail.' . $type] ?? []);
+        $address = Arr::get($config, $type, $this->app->config['mail'][$type] ?? []);
 
         if (is_array($address) && isset($address['address'])) {
             $mailer->{'always' . Str::studly($type)}($address['address'], $address['name']);
