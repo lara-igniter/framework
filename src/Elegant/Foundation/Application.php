d3bd9f72989034af8795d46bcbe2b22b9907d79e
George Tsachrelias <tsaxre1991@hotmail.com>
2024-09-17 10:52:07 +0300

    feat: Update version

--- src/Elegant/Foundation/Application.php
+++ src/Elegant/Foundation/Application.php
@@ -12,7 +12,7 @@ class Application
      *
      * @var string
      */
-    const VERSION = '1.28.1';
+    const VERSION = '1.28.2';
 
     /**
      * The base path for the Laraigniter installation.
