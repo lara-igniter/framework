f5a594321c6008f13fc150938bcf85e473aa0f7b
George Tsachrelias <tsaxre1991@hotmail.com>
2024-09-18 10:32:12 +0300

    refactor: Add view facade code-block functions

--- src/Elegant/Support/Facades/View.php
+++ src/Elegant/Support/Facades/View.php
@@ -2,6 +2,17 @@
 
 namespace Elegant\Support\Facades;
 
+/**
+ * @method static \Elegant\Contracts\View\Factory addNamespace(string $namespace, string|array $hints)
+ * @method static \Elegant\Contracts\View\View first(array $views, \Elegant\Contracts\Support\Arrayable|array $data = [], array $mergeData = [])
+ * @method static \Elegant\Contracts\View\Factory replaceNamespace(string $namespace, string|array $hints)
+ * @method static \Elegant\Contracts\View\View file(string $path, array $data = [], array $mergeData = [])
+ * @method static \Elegant\Contracts\View\View make(string $view, array $data = [], array $mergeData = [])
+ * @method static bool exists(string $view)
+ * @method static mixed share(array|string $key, $value = null)
+ *
+ * @see \Elegant\View\Factory
+ */
 class View extends Facade
 {
     /**
