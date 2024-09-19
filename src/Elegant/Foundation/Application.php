c872faca6cf98181e82f69e2dc0e528e7a3c9a8f
George Tsachrelias <tsaxre1991@hotmail.com>
2024-09-19 08:45:02 +0300

    refactor: Add route facade allow string route name

--- src/Elegant/Foundation/Application.php
+++ src/Elegant/Foundation/Application.php
@@ -12,7 +12,7 @@ class Application
      *
      * @var string
      */
-    const VERSION = '1.29.6';
+    const VERSION = '1.29.7';
 
     /**
      * The base path for the Laraigniter installation.
--- src/Elegant/Support/Facades/Route.php
+++ src/Elegant/Support/Facades/Route.php
@@ -5,12 +5,12 @@ namespace Elegant\Support\Facades;
 use Elegant\Routing\RouteBuilder;
 
 /**
- * @method static \Elegant\Routing\Route get(string|array $methods, \Closure|array $route)
- * @method static \Elegant\Routing\Route post(string|array $methods, \Closure|array $route)
- * @method static \Elegant\Routing\Route put(string|array $methods, \Closure|array $route)
- * @method static \Elegant\Routing\Route patch(string|array $methods, \Closure|array $route)
- * @method static \Elegant\Routing\Route delete(string|array $methods, \Closure|array $route)
- * @method static \Elegant\Routing\Route options(string|array $methods, \Closure|array $route)
+ * @method static \Elegant\Routing\Route get(string|array $methods, \Closure|array|string $route)
+ * @method static \Elegant\Routing\Route post(string|array $methods, \Closure|array|string $route)
+ * @method static \Elegant\Routing\Route put(string|array $methods, \Closure|array|string $route)
+ * @method static \Elegant\Routing\Route patch(string|array $methods, \Closure|array|string $route)
+ * @method static \Elegant\Routing\Route delete(string|array $methods, \Closure|array|string $route)
+ * @method static \Elegant\Routing\Route options(string|array $methods, \Closure|array|string $route)
  * @method static void group(string $prefix, callable|array $attributes, callable|null $routes = null)
  * @method static void middleware(mixed $middleware, string $point = 'pre_controller')
  * @method static void compileAll()
