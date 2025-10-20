dd81874f0babbaf9e627610ab758e167a0673200
George Tsachrelias <tsaxre1991@hotmail.com>
2025-10-20 14:26:38 +0300

    feat: Application version to 1.52.11

--- src/Elegant/Foundation/Application.php
+++ src/Elegant/Foundation/Application.php
@@ -12,7 +12,7 @@ class Application implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '1.52.10';
+    const VERSION = '1.52.11';
 
     /**
      * The base path for the Laraigniter installation.
