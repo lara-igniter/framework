ecfa1412a9f67b58bd9fbfed381ec920d71789d5
George Tsachrelias <tsaxre1991@hotmail.com>
2026-03-25 18:24:36 +0200

    chore: Bump application version to 1.59.2

--- src/Elegant/Foundation/Application.php
+++ src/Elegant/Foundation/Application.php
@@ -12,7 +12,7 @@ class Application implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '1.59.1';
+    const VERSION = '1.59.2';
 
     /**
      * The base path for the Laraigniter installation.
