15bf09888d3831765625c7351ffa126a3d15ba73
George Tsachrelias <tsaxre1991@hotmail.com>
2026-07-28 09:30:37 +0300

    feat: Update version to 1.62.0

--- src/Elegant/Foundation/Application.php
+++ src/Elegant/Foundation/Application.php
@@ -12,7 +12,7 @@ class Application implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '1.61.0';
+    const VERSION = '1.62.0';
 
     /**
      * The base path for the Laraigniter installation.
